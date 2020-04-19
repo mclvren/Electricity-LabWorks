bbf5b048b220ba81db37c524f4676e126aa4cf80
drew <aiv-n@ya.ru>
2020-04-19 22:15:27 +0300

    Осталось сделать смещение

--- Assets/Scripts/LR1.cs
+++ Assets/Scripts/LR1.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,49 +12,105 @@ public class LR1 : MonoBehaviour
     [SerializeField] GameObject Init;
     [SerializeField] GameObject Polar;
     [SerializeField] GameObject Menu;
-    [SerializeField] GameObject Slider;
     [SerializeField] GameObject Current;
+    [SerializeField] GameObject Plus;
+    [SerializeField] GameObject Minus;
+    [SerializeField] GameObject Zero;
+    [SerializeField] GameObject Blackout;
 
     Coroutine coroutine;
     private bool isOn = false;
-    private float mA;
+    private float mA = 0;
     private Text cur;
-    private Slider sl;
     bool goRotatePlr = false;
     private float currentAngle = 0f;
     private float currentValue;
+    private bool inverseState = false;
 
     void Start()
     {
         Init.GetComponent<Button>().onClick.AddListener(Enable);
+        Plus.GetComponent<Button>().onClick.AddListener(PlusX);
+        Minus.GetComponent<Button>().onClick.AddListener(MinusX);
+        Zero.GetComponent<Button>().onClick.AddListener(ZeroX);
+        Polar.GetComponent<Button>().onClick.AddListener(Inverse);
         Deg.GetComponent<Text>().text = "0°";
         cur = Current.GetComponent<Text>();
-        sl = Slider.GetComponent<Slider>();
-        sl.onValueChanged.AddListener(delegate { ValueChanged(); });
         rttclockwise(0);
     }
 
-    void ValueChanged()
+    void PlusX()
     {
-        mA = sl.value;
+        mA += 15;
+        currentValue = mA;
+        cur.text = mA.ToString();
+        StopAllCoroutines();
+        if (isOn) rttclockwise(mA * 0.195f);
+    }
+
+    void MinusX()
+    {
+        if (currentValue > 0)
+        {
+            mA -= 15;
+            currentValue = mA;
+            cur.text = mA.ToString();
+            StopAllCoroutines();
+            if (isOn) rttcounterclockwise(mA * 0.195f);
+        }
+    }
+
+    void ZeroX()
+    {
+        mA = 0;
+        StopAllCoroutines();
         if (mA > currentValue)
         {
             currentValue = mA;
             cur.text = mA.ToString();
-            if (isOn) rttclockwise(mA / 10); // тест
+            if (isOn) rttclockwise(0); // тест
         }
         else
         {
+            currentValue = mA;
             cur.text = mA.ToString();
             //Debug.Log("обратно не робит");
-            if (isOn) rttcounterclockwise(mA / 10);
+            if (isOn) rttcounterclockwise(0);
         }
+    }
 
+    void Inverse()
+    {
+        float k;
+        mA *= -1;
+        inverseState = !inverseState;
+        StopAllCoroutines();
+        if (inverseState) k = 0.2046f;
+        else k = 0.195f;
+        if (mA > currentValue)
+        {
+            currentValue = mA;
+            cur.text = mA.ToString();
+            if (isOn) rttclockwise(mA * k);
+        }
+        else if (mA < currentValue)
+        {
+            currentValue = mA;
+            cur.text = mA.ToString();
+            if (isOn) rttcounterclockwise(mA * k);
+        }
     }
 
     void Update()
     {
-        
+        if (isOn&&(mA != 0))
+        {
+            toggleBlackout(true);
+        }
+        else
+        {
+            toggleBlackout(false);
+        }
     }
 
     IEnumerator RotateMeNow(float targetAngle, float rotationSpeed)
@@ -101,7 +159,7 @@ public class LR1 : MonoBehaviour
     void rttclockwise(float ang)
     {
         var targetAngle = ang;
-        float rotationSpeed = 3f; // Скорость поворота
+        float rotationSpeed = 50f; // Скорость поворота
         StartCoroutine(RotateMeNow(targetAngle, rotationSpeed));
         Deg.GetComponent<Text>().text = (ang.ToString()+"°");
     }
@@ -109,7 +167,7 @@ public class LR1 : MonoBehaviour
     void rttcounterclockwise(float ang)
     {
         var targetAngle = ang;
-        float rotationSpeed = -3f; // Скорость поворота
+        float rotationSpeed = -50f; // Скорость поворота
         StartCoroutine(RotateMeNowCC(targetAngle, rotationSpeed));
         Deg.GetComponent<Text>().text = (ang.ToString() + "°");
     }
@@ -120,12 +178,43 @@ public class LR1 : MonoBehaviour
         {
             isOn = true;
             Init.GetComponentInChildren<Text>().text = "Выключить";
+            StopAllCoroutines();
+            if (mA != 0)
+            {
+                rttclockwise(mA * 0.195f);
+            }
         }
         else if (isOn == true)
         {
             isOn = false;
             Init.GetComponentInChildren<Text>().text = "Включить";
+            mA = 0;
+            StopAllCoroutines();
+            if (mA > currentValue)
+            {
+                currentValue = mA;
+                cur.text = mA.ToString();
+                rttclockwise(0); // тест
+            }
+            else
+            {
+                currentValue = mA;
+                cur.text = mA.ToString();
+                rttcounterclockwise(0);
+            }
         }
     }
 
+    void toggleBlackout(bool state) {
+        if (state)
+        {
+            Blackout.transform.gameObject.SetActive(true);
+            Deg.transform.gameObject.SetActive(true);
+        }
+        else
+        {
+            Blackout.transform.gameObject.SetActive(false);
+            Deg.transform.gameObject.SetActive(false);
+        }
+    }
 }
