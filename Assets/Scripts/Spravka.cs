10951c35ad68a64aa3c49c3f6683a61e1a9a6277
Kru6ka <rty3ueh6eva@gmail.com>
2020-06-26 22:27:34 +0300

    Справки v0.1
    Добавил кнопки для справок

---
+++ Assets/Scripts/Spravka.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LR2_2()
+    {
+
+    }
+
+    public void LR2_5()
+    {
+
+    }
+
+    public void LR2_9()
+    {
+
+    }
+
+    public void LR2_11()
+    {
+
+    }
+}
